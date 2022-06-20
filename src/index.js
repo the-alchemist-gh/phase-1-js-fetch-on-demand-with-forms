62182f14c7a71a1cdd0c9dc099343d06962f8272
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-06-20 12:18:37 +0000

    Completed assignments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const search = document.querySelector('input#searchByID');
+    console.log(search.value);
+    fetch(`http://localhost:3000/movies/${search.value}`)
+    .then(resp => resp.json())
+    .then(data => {
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    })
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
